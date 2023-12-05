052461e7667357baae77cda5ec9ad53bd08a0702
Sergio Hidalgo <shidalgoc@auna.pe>
2023-12-05 09:28:32 -0500

    modelo de datos, modelo de dominio, plantilla

--- 04-plantilla/src/index.ts
+++ 04-plantilla/src/index.ts
@@ -1,14 +1,14 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-import app from './app';
-import { MysqlBootstrap } from './bootstrap/mysql.bootstrap';
-import { RabbitmqBootstrap } from './bootstrap/rabbitmq.bootstrap';
-import { ServerBootstrap } from './bootstrap/server.bootstrap';
+import app from "./app";
+import { MysqlBootstrap } from "./bootstrap/mysql.bootstrap";
+import { RabbitmqBootstrap } from "./bootstrap/rabbitmq.bootstrap";
+import { ServerBootstrap } from "./bootstrap/server.bootstrap";
 
 dotenv.config();
 const serverBootstrap = new ServerBootstrap(app);
 const mysqlBootstrap = new MysqlBootstrap();
-const rabbitmqBootstrap = new RabbitmqBootstrap();git 
+const rabbitmqBootstrap = new RabbitmqBootstrap();
 
 (async () => {
   try {
